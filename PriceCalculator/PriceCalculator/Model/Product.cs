e665c76171d7aa3f190d725ca8b9a4b59e6f91da
Edgaras Valius <edgaras.valius@googlemail.com>
2021-02-22 21:42:25 +0000

    1. TAX - Product model added

---
+++ PriceCalculator/PriceCalculator/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculator.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int UPC { get; set; }
+        public decimal Price { get; set; }
+        public string GetProductTaxMessage(decimal tax)
+        {
+            decimal taxPrice = Price + (Price / 100 * tax);
+            return String.Format("Product price reported as ${0} before tax and ${1} after {2}% tax.", Price.ToString("N2"), taxPrice.ToString("N2"), tax.ToString("0.##"));
+        }
+    }
+}
--- PriceCalculator/PriceCalculator/Program.cs
+++ PriceCalculator/PriceCalculator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PriceCalculator.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,22 @@ namespace PriceCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        private static decimal Tax = 20;
+        private static List<Product> Products;
+        private static void Main(string[] args)
         {
             //Lets begin!
+            Initialise();
+            Console.WriteLine(Products[0].GetProductTaxMessage(Tax));
+            Console.WriteLine(Products[0].GetProductTaxMessage(Tax + 1));
+
+            Console.ReadLine();
         }
+
+        private static void Initialise()
+        {
+            Products = new List<Product>() { new Product { Name = "The little Prince", Price = 20.25m, UPC = 12345 } };
+        }
+
     }
 }
